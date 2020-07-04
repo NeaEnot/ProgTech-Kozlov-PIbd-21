7a6d83c49bd9cbe6856eb3a73227d7bfd376d247
NeaEnot <54849648+NeaEnot@users.noreply.github.com>
2020-07-04 09:19:29 +0400

    Update ClientLogic.cs

--- ReinforcedConcreteFactoryDatabaseImplement/Implements/ClientLogic.cs
+++ ReinforcedConcreteFactoryDatabaseImplement/Implements/ClientLogic.cs
@@ -18,7 +18,7 @@ namespace ReinforcedConcreteFactoryDatabaseImplement.Implements
 
                 if (element != null)
                 {
-                    throw new Exception("Уже есть клиент с таким названием");
+                    throw new Exception("Уже есть клиент с такой почтой");
                 }
 
                 if (model.Id.HasValue)
